111f19f646fa09875fd866e3ec2d0c47ebfdc508
decodewithParth <parthc2002@gmail.com>
2024-04-11 16:17:22 +0530

    all files moved to practice folder

--- Parth/Practice/tasty-table/src/App.jsx
+++ Parth/Practice/tasty-table/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import SearchRecipeByName from './components/SearchRecipeByName'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <SearchRecipeByName/>
       <h1>Parth Chauhan</h1>
     </>
   )
