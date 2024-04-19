94ca35fdf36a43469b1919f907d6f7da78e071f5
decodewithParth <parthc2002@gmail.com>
2024-04-19 14:17:16 +0530

    5:13:50 completed upto this

--- Parth/details.js
+++ Parth/details.js
@@ -56,9 +56,55 @@ fetchData(null, data => {
     })
 
     ingredients.map(ingredient => {
-
+        ingredientItems += `<li class="ingr-item">${ingredient}</li>
+        `
     });
 
+    $detailContainer.innerHTML = `
+    <figure class="detail-banner img-holder">
+    <img src="${bannerUrl}" width="${width}" height="${height}" alt="${title}" class="img-cover">
+    </figure>
+    
+    <div class="detail-content">
+        <div class="title-wrapper">
+            <h1 class="display-small">${title ?? "Untitled"}</h1>
 
+            <button class="btn btn-secondary has-state has-icon ${isSaved ? "saved" : "removed"}" onlcick="saceRecipe(this,'${recipeId}')">
+                <span class="material-symbols-outlined bookmark-add" aria-hidden="true">bookmark_add</span>
+                <span class="material-symbols-outlined bookmark" aria-hidden="true">bookmark</span>
+                <span class="label-large save-text">Save</span>
+                <span class="label-large unsaved-text">Unsaved</span>
+                </button>
+        </div>
+        <div class="detail-author label-large">
+            <span class="span">by</span>${author}
+        </div>
+        
+        <div class="detail-stats">
+            <div class="stats-item">
+                <span class="display-medium">${ingredients.length}</span>
+                <span class="label-medium">Ingredients</span>
+            </div>
+                
+            <div class="stats-item">
+                <span class="display-medium">${getTime(cookingTime).time || "<1"}</span>
+                <span class="label-medium">${getTime(cookingTime).timeUnit}</span>
+            </div>
 
-})
+            <div class="stats-item">
+                <span class="display-medium">${Math.floor(calories)}</span>
+                <span class="label-medium">Calories</span>
+            </div>
+            
+        </div>
+
+        ${tagElements ? `<div class="tag-list">${tagElements}</div>` : ""}
+            <h2 class="title-medium ingr-title">
+            Ingredients
+            <span class="label-medium">for 6 servings</span>
+            </h2>
+
+            <ul class="body-large ingr-list"></ul>
+        </div>
+        `
+});
